8100bbbf6bec11a006d24093d9f727c00fb1272a
Nestor Custodio <sakimorix@gmail.com>
2017-08-10 12:28:57 -0500

    load/manage contacts via API

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts';
+import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
   state = {
-    contacts: [
-      {
-        "id": "ryan",
-        "name": "Ryan Florence",
-        "email": "ryan@reacttraining.com",
-        "avatarURL": "http://localhost:5001/ryan.jpg"
-      },
-      {
-        "id": "michael",
-        "name": "Michael Jackson",
-        "email": "michael@reacttraining.com",
-        "avatarURL": "http://localhost:5001/michael.jpg"
-      },
-      {
-        "id": "tyler",
-        "name": "Tyler McGinnis",
-        "email": "tyler@reacttraining.com",
-        "avatarURL": "http://localhost:5001/tyler.jpg"
-      }
-    ]
+    contacts: []
   };
+
+  componentDidMount() {
+    ContactsAPI.getAll().then((contacts) => {
+      this.setState({ contacts: contacts })
+    });
+  }
 
   deleteContact = (contact) => {
     this.setState((oldState) => {
@@ -31,6 +19,8 @@ class App extends Component {
         contacts: oldState.contacts.filter((i) => { return (i.id !== contact.id) })
       };
     });
+
+    ContactsAPI.remove(contact);
   };
 
   render() {
